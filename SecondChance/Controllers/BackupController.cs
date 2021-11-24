a4b7c08559e62ddab1a5a069c1c4110afe818c83
DenisSemko <dench327@gmail.com>
2021-11-24 23:26:03 +0200

    Add Backup Functionality

---
+++ SecondChance/Controllers/BackupController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+using Microsoft.Data.SqlClient;
+
+namespace SecondChance.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BackupController : ControllerBase
+    {
+        private IConfiguration configuration { get; set; }
+
+        public BackupController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GenerateBackupFile()
+        {
+            try
+            {
+                await Task.Run(() =>
+                {
+                    string dbConnectionString = configuration.GetConnectionString("DefaultConnection");
+                    string backupDestination = "C:\\SQLBackUpFolder\\";
+
+                    if (!System.IO.Directory.Exists(backupDestination))
+                    {
+                        System.IO.Directory.CreateDirectory("C:\\SQLBackUpFolder");
+                    }
+                    SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder(dbConnectionString);
+                    var backupFileName = $"{backupDestination}{sqlConnectionStringBuilder.InitialCatalog}-{DateTime.Now.ToString("yyyy-MM-dd")}.bak";
+                    if (System.IO.File.Exists(backupFileName))
+                        System.IO.File.Delete(backupFileName);
+                    using (SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString))
+                    {
+                        string backupQuery = $"BACKUP DATABASE {sqlConnectionStringBuilder.InitialCatalog} TO DISK='{backupFileName}'";
+                        using (SqlCommand command = new SqlCommand(backupQuery, connection))
+                        {
+                            connection.Open();
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                });
+                return StatusCode(200);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
