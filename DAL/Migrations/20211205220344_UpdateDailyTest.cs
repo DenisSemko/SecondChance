643b10899ed170e54e008cc2df1e1c18d443dd36
DenisSemko <dench327@gmail.com>
2021-12-06 00:04:03 +0200

    update database

---
+++ DAL/Migrations/20211205220344_UpdateDailyTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class UpdateDailyTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CorrectAnswers",
+                table: "DailyTestResult",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CorrectAnswers",
+                table: "DailyTestResult");
+        }
+    }
+}
